d47972f904ec2202cb011893a0110a3b6ff727a5
Raphael Vrient <rv@restomax.com>
2020-12-25 02:08:20 +0100

    First Commit

---
+++ dist/index.d.ts
@@ -0,0 +1,46 @@
+export interface CordovaWebsocketRecvEventCallback {
+    (event: CordovaWebsocketEvent): void;
+}
+export interface CordovaWebsocketSuccessCallback {
+    (success: CordovaWebsocketSuccess): void;
+}
+export interface CordovaWebsocketErrorCallback {
+    (error: CordovaWebsocketError): void;
+}
+export interface CordovaWebsocketEvent extends CordovaWebsocketError {
+    callbackMethod: string;
+    webSocketId: string;
+    message: string;
+}
+export interface CordovaWebsocketSuccess {
+    webSocketId: string;
+    code: number;
+}
+export interface CordovaWebsocketError extends CordovaWebsocketClose {
+    callbackMethod: string;
+    webSocketId: string;
+    code: number;
+    reason: string;
+    exception?: string;
+}
+export interface CordovaWebsocketClose {
+    callbackMethod: string;
+    webSocketId: string;
+    code: number;
+    reason: string;
+}
+export interface CordovaWebsocketOptions {
+    url: string;
+    timeout?: number;
+    pingInterval?: number;
+    headers?: {
+        [key: string]: any;
+    };
+    acceptAllCerts?: boolean;
+}
+export interface ICordovaWebsocketPlugin {
+    wsConnect(options: CordovaWebsocketOptions, receiveCallback?: CordovaWebsocketRecvEventCallback, successCallback?: CordovaWebsocketSuccessCallback, failureCallback?: CordovaWebsocketErrorCallback): void;
+    wsSend(webSocketId: string, message: string): void;
+    wsClose(webSocketId: string, code?: number, reason?: string): void;
+}
+export declare const CordovaWebsocketPlugin: ICordovaWebsocketPlugin;
